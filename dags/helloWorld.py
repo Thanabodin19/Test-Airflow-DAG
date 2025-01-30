40e19aedd7eb37f04855b635ba159c5ec1287701
Thanabodin19 <thanabodin.kea@gmail.com>
2025-01-30 11:29:44 +0700

    update folder dags

--- dags/helloWorld.py
+++ dags/helloWorld.py
@@ -7,10 +7,10 @@ from airflow.operators.python_operator import PythonOperator
 def print_hello():
     print('Hello world from first Airflow DAG!')
 
-dag = DAG('hello_world', description='Hello World DAG',
+dag = DAG('hello_world_test', description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2024, 12, 12), catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_k8s', python_callable=print_hello, dag=dag)
 
-hello_operator
+print(hello_operator)
